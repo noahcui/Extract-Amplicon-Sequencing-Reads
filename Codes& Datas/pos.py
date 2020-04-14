0a233ed65b5d4829bf6c80b0379b758025598300
noahcui <noahcui@hotmail.com>
2020-04-14 03:43:26 -0400

    a

--- Codes& Datas/pos.py
+++ Codes& Datas/pos.py
@@ -4,15 +4,15 @@ import sys
 import sqlite3
 
 def create_table(c, table_name):
-    cmd = "create or replace table " + table_name + "(INFO,text\
-        SEQ text,\
-        START int,\
-        END int);"
+    cmd = "create table " + table_name + "(File text,\
+        Query text,\
+        Reference text,\
+        Position int);"
     c.execute(cmd)
     print("table created successfully, now inserting datas")
 
 def insert_data(c, table_name, data):
-    cmd = "INSERT INTO " + table_name + "(INFO, SEQ, START, END) VALUES (?,?,?,?);"
+    cmd = "INSERT INTO " + table_name + "(File, Query, Reference, Position) VALUES (?,?,?,?);"
     c.execute(cmd, data)
 
 if __name__ == '__main__':
@@ -23,24 +23,24 @@ if __name__ == '__main__':
     table_name = "startPOS"
     create_table(c, table_name)
     for i in range(len(sys.argv)):
-        if i > 2:
+        if i > 1:
             #directiory
             file_name = sys.argv[i]
-            c = 0
+            count = 0
             with open(file_name, "r") as pos_file:
                 for line in pos_file:
-                    if c % 5 == 0:
+                    if count % 5 == 0:
                         File = line.replace('File: ', '')
-                        c = c + 1
-                    if c % 5 == 1:
+                        count = count + 1
+                    if count % 5 == 1:
                         Query = line.replace('Query: ', '')
-                        c = c + 1
-                    if c % 5 == 2:
+                        count = count + 1
+                    if count % 5 == 2:
                         Reference = line.replace('Reference: ', '')
-                        c = c + 1
-                    if c % 5 == 3:
+                        count = count + 1
+                    if count % 5 == 3:
                         Position = line.replace('Position: ', '')
-                        c = c + 1
+                        count = count + 1
                     else:
                         data = (File, Query, Reference, Position)
                         insert_data(c, table_name, data)
