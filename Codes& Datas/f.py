4c6ee3965d8ede550779bb7d63e96c269b026508
noahcui <noahcui@hotmail.com>
2020-04-14 02:47:32 -0400

    a

--- Codes& Datas/f.py
+++ Codes& Datas/f.py
@@ -30,8 +30,9 @@ if __name__ == '__main__':
                 if Query != '':
                     print("reference: ", Reference)
                     print("query: ", Query)
-                Reference = fields[0]
+                Reference = fields[0].replace('<','')
+                Query = ''
             else:
-                Query = Query + line.replace('/n','')
+                Query = Query + line.replace('\n','')
         print("reference: ", Reference)
         print("query: ", Query)
