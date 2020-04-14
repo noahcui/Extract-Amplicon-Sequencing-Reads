b526454af2196b788d80b95bb9f21889cbaa55e8
noahcui <noahcui@hotmail.com>
2020-04-14 03:06:55 -0400

    a

--- Codes& Datas/f.py
+++ Codes& Datas/f.py
@@ -5,10 +5,8 @@ import sqlite3
 
 
 def create_table(c, table_name):
-    cmd = "create or replace table fasta (INFO,text\
-        SEQ text,\
-        START int,\
-        END int);"
+    cmd = "create or replace table fasta (Reference,text\
+        Query, text);"
     c.execute(cmd)
     print("table created successfully, now inserting datas")
 
@@ -21,18 +19,23 @@ def insert_data(c, table_name, data):
 
 if __name__ == '__main__':
     file_name = "Silva.fasta"
-    c = 0
+    conn = None
+    dbname = sys.argv[1]
+    conn = sqlite3.connect(dbname)
+    c = conn.cursor()
     Query = ''
     with open(file_name, "r") as pos_file:
         for line in pos_file:
             fields = line.split(" ")
             if '>'in line:
                 if Query != '':
-                    print("reference: ", Reference)
-                    print("query: ", Query)
+                    data = (Reference, Query)
+                    insert_data(c, "fasta", data)
                     Query = ''
                 Reference = fields[0].replace('>','')
             else:
                 Query = Query + line.replace('\n','')
-        print("referenceend: ", Reference)
-        print("queryend: ", Query)
+        data = (Reference, Query)
+        insert_data(c, "fasta", data)
+    conn.commit()
+    conn.close()
