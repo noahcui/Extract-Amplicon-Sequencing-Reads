1be8f0afbb03e433a8e7678d6a7fa613dbab63f5
noahcui <noahcui@hotmail.com>
2020-04-14 02:57:59 -0400

    a

--- Codes& Datas/f.py
+++ Codes& Datas/f.py
@@ -26,6 +26,7 @@ if __name__ == '__main__':
         for line in pos_file:
             fields = line.split(" ")
             Query = ''
+            Reference = ''
             if '>'in line:
                 print("reference: ", Reference)
                 print("query: ", Query)
